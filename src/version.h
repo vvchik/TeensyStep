bc87ccbf9d8012946b8ffe3a6251c741145261de
Vladimir Babchynskyy <vvchik@gmail.com>
2021-02-06 00:26:29 -0800

    updated version metadata

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 
 namespace TeensyStep
 {
-    constexpr const char* version = "V2.1";
+    constexpr const char* version = "V2.2.1";
     constexpr const char* branch = "master";
 } // namespace TeensyStep
